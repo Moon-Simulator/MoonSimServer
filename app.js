b8d906573cab54196b8c667601f0b892befc9257
MoonSim206 <moonsim206@gmail.com>
2018-03-19 13:25:15 +0900

    add crop endpoint

--- app.js
+++ app.js
@@ -2,20 +2,28 @@ var express = require('express');
 var app = express();
 
 const gm = require('gm');
-const width = 300;
-const height = 400;
+const fs = require('fs');
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-gm('./assets/moon_1024.jpg')
-	.crop(width, height)
-	.write('./output/temp.jpg', (err) => {
-		if (err) {
-			console.log(err);
-		}
-	});
+app.get('/crop/:width/:height', (request, response) => {
+  
+  const { width, height } = request.params; // Get the width and height from the request parameters
+  
+  gm('./assets/moon_1024.jpg')
+    .crop(width, height)
+    .write('./output/tmp.png', (err) => {
+      if (err) {
+        console.log(err); 
+      } else {
+        response.sendFile('./output/tmp.png');
+      }
+    })
+
+   fs.unlinkSync('./tmp.png'); // Delete the temporary file that we created in the cropping task
+});
 
 app.listen(3000, function () {
   console.log('Moon Simulator listening on port 3000!');
