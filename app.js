bf3c6315cd560e7f4ffa5db56a71773b9e94e518
MoonSim206 <moonsim206@gmail.com>
2018-03-19 13:35:27 +0900

    add gm

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.get('/crop/:width/:height', (request, response) => {
       }
     })
 
-   fs.unlinkSync('./output/tmp.png'); // Delete the temporary file that we created in the cropping task
+   fs.unlinkSync('/home/ubuntu/moonsimserver/output/tmp.png'); // Delete the temporary file that we created in the cropping task
 });
 
 app.listen(3000, function () {
