25f8b2425086e6659aaee1ddff43071b05ec85d1
MoonSim206 <moonsim206@gmail.com>
2018-03-19 13:11:32 +0900

    add gm

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var app = express();
 
+var gm = require('gm');
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+// gm('/')
+
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Moon Simulator listening on port 3000!');
 });
